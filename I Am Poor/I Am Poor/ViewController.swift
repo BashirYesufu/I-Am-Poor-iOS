649ef85dc71900079ff72c0607eb44b2e54e1fa8
Bashir Yesufu <yesufubashir@gmail.com>
2021-12-25 16:24:38 +0100

    Added all files

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by mac on 12/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
